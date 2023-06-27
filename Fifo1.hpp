511764128866217bdf04a7b02eee3346805ddff9
CharlesFrasch <51298243+CharlesFrasch@users.noreply.github.com>
2023-06-27 15:55:51 -0400

    Add files via upload

---
+++ Fifo1.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <cstdlib>
+#include <memory>
+
+
+/// Non-threadsafe circular FIFO
+template<typename T>
+class Fifo1
+{
+public:
+    using ValueType = T;
+
+    explicit Fifo1(std::size_t size)
+        : size_{size}
+        , ring_{
+              static_cast<ValueType*>(std::aligned_alloc(alignof(T), size * sizeof(T))),
+              &std::free}
+    {}
+
+
+    std::size_t size() const { return size_; }
+    bool empty() const { return popCursor_ == pushCursor_; }
+    bool full() const { return (pushCursor_ - popCursor_) == size_; }
+
+    bool push(T const& value) {
+        if (full()) {
+            return false;
+        }
+        new (ring_.get() + pushCursor_ % size_) T(value);
+        ++pushCursor_;
+
+        return true;
+    }
+
+    /// Pop one object from the fifo.
+    /// @return `true` if the pop operation is successful; `false` if fifo was empty.
+    bool pop(T& value) {
+        if (empty()) {
+            return false;
+        }
+        value = ring_.get()[popCursor_ % size_];
+        ring_.get()[popCursor_ % size_].~T();
+        ++popCursor_;
+
+        return true;
+    }
+
+private:
+    std::size_t size_;
+
+    using RingType = std::unique_ptr<ValueType, decltype(&std::free)>;
+    RingType ring_;
+
+    std::size_t pushCursor_{};
+    std::size_t popCursor_{};
+};
---
+++ Fifo1_ut.cpp
@@ -0,0 +1,59 @@
+#include "Fifo1.hpp"
+
+#include <gtest/gtest.h>
+
+#include <type_traits>
+
+
+using TestFifo = Fifo1<int>;
+
+TEST(Fifo1, initialConditions) {
+    auto fifo = TestFifo(1234);
+
+    EXPECT_EQ(1234u, fifo.size());
+    EXPECT_TRUE(fifo.empty());
+    EXPECT_FALSE(fifo.full());
+}
+
+TEST(Fifo1, push) {
+    auto fifo = TestFifo(4);
+    ASSERT_EQ(4u, fifo.size());
+
+    EXPECT_TRUE(fifo.push(42));
+    EXPECT_FALSE(fifo.empty());
+    EXPECT_FALSE(fifo.full());
+
+    EXPECT_TRUE(fifo.push(42));
+    EXPECT_FALSE(fifo.empty());
+    EXPECT_FALSE(fifo.full());
+
+    EXPECT_TRUE(fifo.push(42));
+    EXPECT_FALSE(fifo.empty());
+    EXPECT_FALSE(fifo.full());
+
+    EXPECT_TRUE(fifo.push(42));
+    EXPECT_FALSE(fifo.empty());
+    EXPECT_TRUE(fifo.full());
+
+    EXPECT_FALSE(fifo.push(42));
+    EXPECT_FALSE(fifo.empty());
+    EXPECT_TRUE(fifo.full());
+}
+
+TEST(Fifo1, pop) {
+    auto fifo = TestFifo(4);
+
+    auto value = TestFifo::ValueType{};
+    EXPECT_FALSE(fifo.pop(value));
+
+    for (auto i = 0u; i < fifo.size(); ++i) {
+        fifo.push(42 + i);
+    }
+
+    for (auto i = 0u; i < fifo.size(); ++i) {
+        auto value = TestFifo::ValueType{};
+        EXPECT_TRUE(fifo.pop(value));
+        EXPECT_EQ(42+i, value);
+    }
+    EXPECT_FALSE(fifo.pop(value));
+}
