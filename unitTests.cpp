a395ce8e2bf7a79e93782c4fc4bf22cb02e8f458
Charlie Frasch <cfrasch@Charles-PC>
2023-07-07 21:32:34 -0400

    WIP

--- unitTests.cpp
+++ unitTests.cpp
@@ -8,6 +8,18 @@
 
 #include <type_traits>
 
+extern "C" {
+    void __ubsan_on_report() {
+          FAIL() << "Encountered an undefined behavior sanitizer error";
+    }
+    void __asan_on_error() {
+          FAIL() << "Encountered an address sanitizer error";
+    }
+    void __tsan_on_report() {
+          FAIL() << "Encountered a thread sanitizer error";
+    }
+}  // extern "C"
+   //
 
 template<typename FifoT>
 class FifoTest : public testing::Test {
